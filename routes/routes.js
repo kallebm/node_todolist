8f74266fcb51b1b401413501f2c3ca6ad2f26911
Kalleb <kallebeliasmaestri1@gmail.com>
2023-04-24 22:23:43 -0300

    add task  feature with databse integration

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const routes = express.Router();
 const TaskController = require("../controller/TaskController");
 
-routes.get("/", TaskController.getAll);
+routes.get("/", TaskController.getAllTasks);
+routes.post("/create", TaskController.createTask);
 
 module.exports = routes;
