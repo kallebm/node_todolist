ad57b99622868c7d77c2da5267317d21c2332a79
Kalleb <kallebeliasmaestri1@gmail.com>
2023-04-25 01:20:00 -0300

    update and delete tasks with data base intregation

--- routes/routes.js
+++ routes/routes.js
@@ -4,5 +4,8 @@ const TaskController = require("../controller/TaskController");
 
 routes.get("/", TaskController.getAllTasks);
 routes.post("/create", TaskController.createTask);
+routes.get("/getById/:id/:method", TaskController.getById);
+routes.post("/updateOne/:id", TaskController.updateOneTask);
+routes.get("/deleteOne/:id", TaskController.deleteOneTask);
 
 module.exports = routes;
